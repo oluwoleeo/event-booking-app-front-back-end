7d3ae904ecabf835e510638f6636400cdce4e7fe
Oluwaremi Oluwole <polymathwole@gmail.com>
2025-02-21 14:39:39 +0000

    Change App component to Board

---
+++ tictactoe/src/Game.js
@@ -0,0 +1,16 @@
+import Board from './Board';
+
+import { useState } from "react";
+
+export default function Game(){
+    return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <ol>{/*TODO*/}</ol>
+          </div>
+        </div>
+      );
+}
